37ece08eaf797de0e83f4876e18c7345835bd746
dhanendras <dhanendra.chaudhary@gmail.com>
2020-11-30 23:21:24 +0530

    tried matic

---
+++ web/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import HelloWorld from './HelloWorld';
+
+const App = () => {
+  return (
+  <div className="App">
+  <HelloWorld />
+              </div>
+  );
+};
+
+export default App;
